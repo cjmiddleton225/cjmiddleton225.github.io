1c0261a8b91fe0a51f264fc963d698cde45abe6f
cjmiddleton225 <58363721+cjmiddleton225@users.noreply.github.com>
2021-02-13 19:56:00 +0000

    Update sw.js
    update service worker

--- Anastasia/sw.js
+++ Anastasia/sw.js
@@ -1,6 +1,13 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsTCache = [
   '/Anastasia/Anastasia.html',
+  '/Anastasia/1.jpg',
+  '/Anastasia/2.jpg',
+  '/Anastasia/3.jpg',
+  '/Anastasia/4.jpg',
+  '/Anastasia/5.jpg',
+  '/Anastasia/6.jpg',
+  '/Anastasia/stylesheets/style.css',
 ];
 console.log('adding install listener')
 self.addEventListener('install', (e) => {
