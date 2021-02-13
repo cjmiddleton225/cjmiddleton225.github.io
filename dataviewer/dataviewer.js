9d65be999f56736d6f64d2ce8770b8a921155424
cjmiddleton225 <cjmiddleton225@hotmail.com>
2021-02-13 19:48:47 +0000

    Anastasia update v day

---
+++ dataviewer/dataviewer.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("appText").innerHTML = "Anastasia is pretty";
+  }
+  function myFunction2() {
+      var a = document.getElementById("sideBarButton");
+    document.getElementById("appText").innerHTML = "And cool!";
+  }
