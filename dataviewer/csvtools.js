47d5f701ba12be7b4ea0a4fc1bb35d8fd725ca66
cjmiddleton225 <cjmiddleton225@hotmail.com>
2021-03-08 21:37:45 +0000

    updated loadcsv code

--- dataviewer/csvtools.js
+++ dataviewer/csvtools.js
@@ -1,9 +1,28 @@
-function readCSV(filename) {
-        var reader = new FileReader();
+class CSVReader{
+  filename;
+  rawData;
+  fileLoaded = false;
+  
+  LoadData(filename){
+    this.filename=filename;
+    this.ReadCSV(this.filename);
+  }
+  
+  async ReadCSV(filename) {
+    this.filename=filename;
+        const sleep = function (ms) {
+          return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        let reader = new FileReader();
+        let that=this;
         reader.onload = function () {
-            document.getElementById('CSVRawText').textContent = reader.result;
+            that.rawData = reader.result;
+            that.fileLoaded=true;
         };
         // start reading the file. When it is done, calls the onload event defined above.
         reader.readAsText(filename);
-    };
-
+        while (this.fileLoaded === false) {
+          await sleep(100);
+        }
+    }
+}
--- dataviewer/dataviewer.js
+++ dataviewer/dataviewer.js
@@ -1,12 +1,30 @@
-function myFunction() {
-    document.getElementById("appText").innerHTML = `<p>Select local CSV File:</p>
-    <input id="csv" type="file" accept=".csv">
-    <div>
-    <button onClick="loadCSV_buttonCLick()">Submit</button>
-    </div>
-    <div class="code">
-    <pre id="CSVRawText" >No data selected.</pre>
-      </div>`;
+let CSVData = new CSVReader();
+
+function LoadDataApp() {
+    let appText = document.getElementById("appText");
+    appText.replaceChildren(); // clears the window
+    let inputText = document.createElement("p");
+    inputText.appendChild(document.createTextNode("Select local CSV file:"));
+    let input=document.createElement("input");
+    input.id="csv";
+    input.type="file";
+    input.accept=".csv";
+    let loadCSVDiv = document.createElement("div");
+    let loadCSVButton = document.createElement("button");
+    loadCSVButton.onclick=function () {
+                loadCSV_buttonClick();
+            };
+    loadCSVButton.innerText="Submit";
+    
+    let previewBox = document.createElement("div");
+    previewBox.classList.add("code");
+    previewBox.innerHTML='<pre id="CSVRawText" >No data selected.</pre>';
+    
+    appText.appendChild(inputText);
+    appText.appendChild(input);
+    loadCSVDiv.appendChild(loadCSVButton);
+    appText.appendChild(loadCSVDiv);
+    appText.appendChild(previewBox);
     
   }
   function myFunction2() {
@@ -14,7 +32,13 @@ function myFunction() {
     document.getElementById("appText").innerHTML = "And cool!";
   }
 
-  function loadCSV_buttonCLick(){
+  async function loadCSV_buttonClick(){
     let fileInput = document.getElementById("csv");
-    fileInput.addEventListener('change', readCSV(fileInput.files[0]));
+    await CSVData.ReadCSV(fileInput.files[0]);
+    updateCSVPreview(10);
   }
+  
+  function updateCSVPreview(lines){
+    document.getElementById('CSVRawText').textContent = CSVData.rawData;
+  }
+  
